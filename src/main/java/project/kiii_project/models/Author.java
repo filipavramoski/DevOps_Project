0c68a41fbb78525e627b7e1d719eb89bdbc521d6
filipavramoski <109027330+filipavramoski@users.noreply.github.com>
2025-02-10 14:42:00 +0100

    authors

--- src/main/java/project/kiii_project/models/Author.java
+++ src/main/java/project/kiii_project/models/Author.java
@@ -4,7 +4,7 @@ package project.kiii_project.models;
 import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
-
+//chanheeees
 @Data
 @Document(collection = "authors")
 public class Author {
